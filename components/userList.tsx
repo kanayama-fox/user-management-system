67107b5e6797a49796396ea58da68ef22c1520ad
kanayama-fox <taiki.kanayama@fox-hound.jp>
2025-08-13 11:23:33 +0900

    Day2 タスク1-1完了

---
+++ components/userList.tsx
@@ -0,0 +1,22 @@
+// src/components/UserList.tsx
+import React from "react";
+import { User } from "../types/User"; //// タスク2: User型をインポート
+import UserCard from "./UserCard"; //タスク4-1: UserCardコンポーネントをインポート
+
+interface UserListProps {
+  users: User[];
+}
+
+// タスク3-1 & 3-2: UserListコンポーネント作成、propsを受け取り、users配列をmapで一覧表示
+const UserList: React.FC<UserListProps> = ({ users }) => {
+  return (
+    <div>
+      {/* タスク4-2: users配列の各ユーザー情報をUserCardコンポーネントに渡して表示 */}
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
