f8cb81db8f18d2ddbe7f64016e827860c83f8be6
kanayama-fox <taiki.kanayama@fox-hound.jp>
2025-08-13 12:12:02 +0900

    Day2 タスク1-3完了

--- components/UserList.stories.tsx
+++ components/UserList.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import UserList from "./userList";
+import UserList from "./UserList";
 import { User } from "../types/User";
 
 const meta: Meta<typeof UserList> = {
