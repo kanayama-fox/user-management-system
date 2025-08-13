887cd93b62dc5ea2dc00d614e823850ba2171cac
kanayama-fox <taiki.kanayama@fox-hound.jp>
2025-08-13 11:40:22 +0900

    Day2　タスク1-2完了

---
+++ components/UserList.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import UserList from "./userList";
+import { User } from "../types/User";
+
+const meta: Meta<typeof UserList> = {
+  title: "Components/UserList", // ②タイトル指定
+  component: UserList, // ②コンポーネント指定
+  tags: ["autodocs"], // ②自動ドキュメント対応
+};
+
+export default meta;
+type Story = StoryObj<typeof UserList>;
+
+const sampleUsers: User[] = [
+  {
+    id: 1,
+    name: "山田太郎",
+    email: "taro@example.com",
+    role: "admin",
+    deleted: false,
+  },
+  {
+    id: 2,
+    name: "鈴木花子",
+    email: "hanako@example.com",
+    role: "user",
+    deleted: false,
+  },
+];
+export const Default: Story = {
+  args: {
+    users: sampleUsers,
+  },
+};
